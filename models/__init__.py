be91440f1e4583096460fe074a0858ec9d7af114
Paul Balanca <paul.balanca@gmail.com>
2018-02-28 16:14:59 +0000

    UPDATE: copyright headers...

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,18 @@
-# =========================================================================== #
-# [2017] - Robik AI Ltd - Paul Balanca
-# All Rights Reserved.
-
-# NOTICE: All information contained herein is, and remains
-# the property of Robik AI Ltd, and its suppliers
-# if any.  The intellectual and technical concepts contained
-# herein are proprietary to Robik AI Ltd
-# and its suppliers and may be covered by U.S., European and Foreign Patents,
-# patents in process, and are protected by trade secret or copyright law.
-# Dissemination of this information or reproduction of this material
-# is strictly forbidden unless prior written permission is obtained
-# from Robik AI Ltd.
-# =========================================================================== #
+# ==============================================================================
+# Copyright 2018 The TensorFlow Authors aud Paul Balanca. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -49,14 +50,14 @@ IMAGENET_MODELS = {
     'resnet_v2_152_299': lambda: resnet_v2.ResNet_v2_152(299),
     'nasnet_v1_small': lambda: nasnet.NASNetV1Small(),
     'nasnet_v1_large': lambda: nasnet.NASNetV1Large(),
-    'hex_mobilenet_v1': lambda: hex_mobilenet_v1.HexMobileNetV1(
-        ksize=5, regularize_depthwise=False),
-    'hex_mobilenet_v1_5x5': lambda: hex_mobilenet_v1.HexMobileNetV1(
-        ksize=5, regularize_depthwise=False),
-    'hex_mobilenet_v1_5x5_w': lambda: hex_mobilenet_v1.HexMobileNetV1(
-        ksize=5, regularize_depthwise=True),
-    'hex_mobilenet_v1_3x3': lambda: hex_mobilenet_v1.HexMobileNetV1(
-        ksize=3, regularize_depthwise=False),
+    # 'hex_mobilenet_v1': lambda: hex_mobilenet_v1.HexMobileNetV1(
+    #     ksize=5, regularize_depthwise=False),
+    # 'hex_mobilenet_v1_5x5': lambda: hex_mobilenet_v1.HexMobileNetV1(
+    #     ksize=5, regularize_depthwise=False),
+    # 'hex_mobilenet_v1_5x5_w': lambda: hex_mobilenet_v1.HexMobileNetV1(
+    #     ksize=5, regularize_depthwise=True),
+    # 'hex_mobilenet_v1_3x3': lambda: hex_mobilenet_v1.HexMobileNetV1(
+    #     ksize=3, regularize_depthwise=False),
 }
 
 def create_model(model_name, dataset):
