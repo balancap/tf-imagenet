c92713f14fdd7a5d118354e452323981073e7981
Paul B <paul.balanca@gmail.com>
2018-04-03 12:42:11 +0100

    UPDATE: hex tools.

--- models/mobilenet/hex_tools.py
+++ models/mobilenet/hex_tools.py
@@ -51,12 +51,23 @@ def hex_average_pooling2d(
         # Actually apply depthwise conv instead of separable conv.
         dtype = inputs.dtype.base_dtype
         kernel_h, kernel_w = utils.two_element_tuple(kernel_size)
+        radius = kernel_h // 2
+        if data_format == 'NHWC':
+            num_filters_in = utils.last_dimension(inputs.get_shape(), min_rank=4)
+        else:
+            # No NCHW for now...
+            raise NotImplementedError()
 
+        d = [1., 7., 19., 37.]
+        shape = [kernel_h, kernel_w, num_filters_in, 1]
+        w = tf.constant(
+            1. / d[radius-1],
+            dtype=dtype,
+            shape=shape,
+            name='Const')
         # Constant weights for average pooling.
-        outputs = nn.hex_depthwise_conv2d(inputs,
-                                          depthwise_weights,
-                                          strides,
-                                          padding,
+        outputs = nn.hex_depthwise_conv2d(inputs, w,
+                                          [1, 1, 1, 1], 'SAME',
                                           data_format=data_format)
         return utils.collect_named_outputs(outputs_collections,
                                            sc.original_name_scope, outputs)
